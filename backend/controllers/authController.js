d6644b9b69630e6c5553eedd6e067a450addfaf2
Somen Saini <sainisomen@gmail.com>
2023-09-09 16:27:07 -0400

    registration POST request fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -66,23 +66,24 @@ const getSignup = (req, res) => {
 };
 
 const postSignup = (req, res, next) => {
-  const validationErrors = [];
-  if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
-  if (!validator.isLength(req.body.password, { min: 8 }))
-    validationErrors.push({
-      msg: "Password must be at least 8 characters long",
-    });
-  // if (req.body.password !== req.body.confirmPassword)
-  //   validationErrors.push({ msg: "Passwords do not match" });
 
-  if (validationErrors.length) {
-    // req.flash("errors", validationErrors);
-    return res.redirect("/register");
-  }
-  req.body.email = validator.normalizeEmail(req.body.email, {
-    gmail_remove_dots: false,
-  });
+  // const validationErrors = [];
+  // if (!validator.isEmail(req.body.email))
+  //   validationErrors.push({ msg: "Please enter a valid email address." });
+  // if (!validator.isLength(req.body.password, { min: 8 }))
+  //   validationErrors.push({
+  //     msg: "Password must be at least 8 characters long",
+  //   });
+  // // if (req.body.password !== req.body.confirmPassword)
+  // //   validationErrors.push({ msg: "Passwords do not match" });
+
+  // if (validationErrors.length) {
+  //   // req.flash("errors", validationErrors);
+  //   return res.redirect("/register");
+  // }
+  // req.body.email = validator.normalizeEmail(req.body.email, {
+  //   gmail_remove_dots: false,
+  // });
 
   const user = new User({
     username: req.body.username,
@@ -98,10 +99,7 @@ const postSignup = (req, res, next) => {
       }
       if (existingUser) {
         console.log("Account with that email address or username already exists.")
-        // req.flash("errors", {
-        //   msg: "Account with that email address or username already exists.",
-        // });
-        return res.redirect("../register");
+        return res.redirect("/");
       }
       user.save((err) => {
         if (err) {
--- frontend/src/features/auth/Register.jsx
+++ frontend/src/features/auth/Register.jsx
@@ -7,6 +7,7 @@ const Register = () => {
     const [ email, setEmail ] = useState()
 
     const usernameUpdate=(e)=>{
+        console.log(e.target.value)
         setUsername(e.target.value)
     }
     const passwordUpdate=(e)=>{
@@ -22,15 +23,16 @@ const Register = () => {
             let res = await fetch("http://localhost:3500/register", {
                 method: "POST",
                 body: JSON.stringify({
-                    username: username,
-                    password: password,
-                    email: email
+                    "username": username,
+                    "password": password,
+                    "email": email
                 }),       
+                headers:{
+                    'Content-Type': 'application/json'
+                }
             });
             let resJson = await res.json()
             if (res.status === 200){
-                setUsername('')
-                setPassword('')
                 console.log('Success')
             }else{
                 console.log('Some error')
@@ -40,43 +42,45 @@ const Register = () => {
         }
     }
 
-        // const postURL = "http://localhost:3500/register"
-        // fetch(postURL, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         username: username,
-        //         password: password,
-        //         email: email
-        //     })
-        // })
-        // .then(()=> {
-        //     console.log("Added")
-        // })
     return (
         <>
             <form onSubmit={handleSubmit} className='register--form'>
-            {/* <form className='register--form' action="/register" method="POST"> */}
                 <h3>Join Letterboxd</h3>
 
                 <div>
                     <label className='login--label'>Username</label>
-                    {/* <input className='login--input' type="text" placeholder="Username" id="username" name="username"/>   */}
-                    <input required onChange={usernameUpdate} 
-                    className='login--input' type="text" placeholder="Username" id="username" name="username"/>  
+                    <input 
+                    required onChange={usernameUpdate} 
+                    className='login--input' 
+                    type="text"
+                    value={username} 
+                    placeholder="Username" 
+                    id="username" 
+                    name="username"/>  
                 </div>
 
                 <div>
                     <label className='login--label'>Email</label>
-                    <input required onChange={passwordUpdate} className='login--input' type="email" placeholder="Email" id="email" name="email"/>
+                    <input 
+                    required onChange={emailUpdate} 
+                    value={email} 
+                    className='login--input' 
+                    type="email" 
+                    placeholder="Email" 
+                    id="email" 
+                    name="email" />
                 </div>
 
                 <div>
                     <label className='login--label'>Password</label>
-                    <input required onChange={emailUpdate} className='login--input' type="password" placeholder="Password" id="password" name="password"/>
+                    <input 
+                    required onChange={passwordUpdate} 
+                    value={password} 
+                    className='login--input' 
+                    type="password" 
+                    placeholder="Password" 
+                    id="password" 
+                    name="password"/>
                 </div>
 
                 <button type="submit" className='login--button'>Register</button>
