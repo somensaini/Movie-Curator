968fa7188f9a10a5d6ba08b7b8f568ec12522954
Somen Saini <sainisomen@gmail.com>
2023-09-26 19:50:34 -0400

    updated Add to Favorites controller to update isFavorite property of user's entry in list

--- backend/controllers/favoriteController.js
+++ backend/controllers/favoriteController.js
@@ -1,14 +1,15 @@
-const User = require('../models/User')
 const List = require('../models/List')
 const asyncHandler = require('express-async-handler')
 
-// Add an entry to a user's list
+// Set an entry's isFavorite value to true
 const updateList = asyncHandler(async (req, res) => {
-    const { username, movieId, isFavorite } = req.body
+    const { username, movieId } = req.body
     try {
         await List.updateOne(
-            {username: username},
-            {$push: {movieList: {movieId: movieId, isFavorite: true}}}         
+            {'username': username, 'movieList.movieId': movieId},
+            {$set: {
+                'movieList.$.isFavorite': true
+            }}
         )
         return res.status(201).json({ message: "The movie was added to the user's database." })
     } catch (err) {
@@ -16,20 +17,6 @@ const updateList = asyncHandler(async (req, res) => {
     }
 })
 
-
-
 module.exports = {
     updateList
 }
-
-
-
-
-
-
-
-
-
-
-
-
