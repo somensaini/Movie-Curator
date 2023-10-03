abe9e471a70a87c437b52d0b3c1de45624f12442
Somen Saini <sainisomen@gmail.com>
2023-10-03 13:13:58 -0400

    ready to deploy backend

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,4 @@
 const allowedOrigins = [
-    'http://localhost:3500',
-    'http://localhost:5173',
-    "https://letterboxd-clone-api.onrender.com",
     "https://letterboxd-clone.onrender.com/"
 ]
 
