0fce5398178f274c86085b0fbf853b86e2d1ca6d
Minjae Cho <minjae1.cho@torontomu.ca>
2023-10-03 15:15:04 -0400

    .

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    "https://movie-curator.onrender.com/",
+    "https://movie-curator.onrender.com",
     "https://movie-curator.onrender.com/*",
     "https://api.themoviedb.org",
     "https://onrender.com"
