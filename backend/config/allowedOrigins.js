013a173784afa95729ca986d8ba2485c8799775a
Minjae Cho <minjae1.cho@torontomu.ca>
2023-10-03 15:22:38 -0400

    .

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,9 @@ const allowedOrigins = [
     "https://movie-curator.onrender.com",
     "https://movie-curator.onrender.com/*",
     "https://api.themoviedb.org",
-    "https://onrender.com"
+    "https://onrender.com",
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
 ]
 
 module.exports = allowedOrigins
