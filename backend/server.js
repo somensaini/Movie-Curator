e2b0a017dce5f019722a3a19566526e40bc24830
Somen Saini <sainisomen@gmail.com>
2023-10-06 01:00:00 -0400

    Removed CORS Header for /login route

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use(passport.session())
 // app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://movie-curator.onrender.com');
-  res.setHeader('Access-Control-Allow-Origin', 'https://movie-curator.onrender.com/login');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://movie-curator.onrender.com/login');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
