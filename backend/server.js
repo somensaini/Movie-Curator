8b81bca3a05a0fa682281a5d844b79a0be825920
Somen Saini <sainisomen@gmail.com>
2023-10-07 13:25:18 -0400

    Configured server to include appropriate headers

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,19 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 // CORS
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://movie-curator.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next()
+  }
+})
 
 // Cookie Parser
 app.use(cookieParser("keyboard cat"))
