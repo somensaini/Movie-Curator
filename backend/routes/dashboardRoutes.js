194e7bf82cf4533158c3edb09922bcb7f0292c65
Somen Saini <sainisomen@gmail.com>
2023-10-07 17:50:22 -0400

    ensureAuth test

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const dashboardController = require('../controllers/dashboardController')
+const { ensureAuth } = require('../middleware/auth')
 
 router.route('/')
-    .get(dashboardController.getDashboard)
+    .get(ensureAuth, dashboardController.getDashboard)
 
 module.exports = router
