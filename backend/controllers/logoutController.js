8a4135d4498396079e55a16d42aa2f25a6a2be58
Somen Saini <sainisomen@gmail.com>
2023-10-23 12:39:00 -0400

    Updated postLogout

--- backend/controllers/logoutController.js
+++ backend/controllers/logoutController.js
@@ -1,10 +1,10 @@
 // POST
 // Logout the User
 const postLogout = (req, res, next) => {
-    req.logout(function(err) {
-        if (err) { return next(err); }
+    // req.logout(function(err) {
+    //     if (err) { return next(err); }
         return res.status(200).json({ message: "Logout Success" });
-    });
+    // });
 };
 
 module.exports = {
