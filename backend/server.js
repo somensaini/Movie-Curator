64434cdcb651d466bde3748f693eb7bb8fb1806a
Somen Saini <sainisomen@gmail.com>
2024-08-04 01:06:02 -0400

    changed session storage

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const app = express();
-const path = require('path')
-const { logger, logEvents } = require('./middleware/logger')
-const errorHandler = require('./middleware/errorHandler')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
-const connectDB = require('./config/database')
-const mongoose = require('mongoose')
-const passport = require('passport')
-require("./config/passport")(passport);
-const session = require("express-session");
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const path = require("path")
+const { logger, logEvents } = require("./middleware/logger")
+const errorHandler = require("./middleware/errorHandler")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const corsOptions = require("./config/corsOptions")
+const connectDB = require("./config/database")
+const mongoose = require("mongoose")
+const passport = require("passport")
+require("./config/passport")(passport)
+// const session = require("express-session");
+const session = require("cookie-session")
 const PORT = process.env.PORT || 3500
 
 // Use the JSON middleware function built into Express. It parses incoming JSON requests and puts the parsed data in req.body
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 //Connect To Database
 connectDB()
@@ -26,7 +27,7 @@ app.use(errorHandler)
 app.use(logger)
 
 // CORS
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 // Express Sessions
 app.use(
@@ -35,7 +36,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
   })
-);
+)
 
 // Passport middleware
 app.use(passport.initialize())
@@ -45,17 +46,17 @@ app.use(passport.session())
 app.use(cookieParser("keyboard cat"))
 
 // This will send the user all static files in the public directory when they are on the root page
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 // Routes
-app.use('/login', require('./routes/loginRoutes'))
-app.use('/dashboard', require('./routes/dashboardRoutes'))
-app.use('/user', require('./routes/userRoutes'))
-app.use('/list', require('./routes/listRoutes'))
-app.use('/favorite', require('./routes/favoriteRoutes'))
-app.use('/register', require('./routes/registerRoutes'))
-app.use('/logout', require('./routes/logoutRoutes'))
-app.use('/', require('./routes/root'))
+app.use("/login", require("./routes/loginRoutes"))
+app.use("/dashboard", require("./routes/dashboardRoutes"))
+app.use("/user", require("./routes/userRoutes"))
+app.use("/list", require("./routes/listRoutes"))
+app.use("/favorite", require("./routes/favoriteRoutes"))
+app.use("/register", require("./routes/registerRoutes"))
+app.use("/logout", require("./routes/logoutRoutes"))
+app.use("/", require("./routes/root"))
 
 // For all other cases it will send the 404 page
 // app.all('*', (req, res) => {
@@ -71,12 +72,15 @@ app.use('/', require('./routes/root'))
 
 // Console log when connected and if any errors occur
 // The event will only be called once
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB')
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB")
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
 
-mongoose.connection.on('error', err => {
-    console.log(err)
-    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+mongoose.connection.on("error", (err) => {
+  console.log(err)
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  )
 })
